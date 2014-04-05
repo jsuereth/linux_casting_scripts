ef1d0fbfc641aee261854b027903c4d926197055
Josh Suereth <joshua.suereth@gmail.com>
2014-04-05 00:27:00 -0400

    First cut at scalawags + twitch streaming scripts for linux.

---
+++ pulse_setup.bash
@@ -0,0 +1,230 @@
+#!/bin/bash
+
+# TODO - Write created devices to a file somewhere and clean that up.
+
+
+# Setups up a pulse-audio feed with microphone + extras going to recording
+# and all the extras still going to the speakers.
+declare -r SCRIPT_NAME=${0##*/}
+
+# Helpers
+
+# Grabs the client id, given a client name we search for.
+paInputSource() {
+  echo $(pactl list short sources | grep -i "$1" | head -1 | awk "{ print \$2 }")
+}
+paClientId() {
+  echo $(pactl list short clients | grep "$1" | head -1 | awk "{print \$1}")
+}
+
+paSinkId() {
+  echo $(pactl list short sinks | grep "$1" | head -1 | awk "{print \$1}")
+}
+
+paClientInputId() {
+  local CLIENT_ID=$(paClientId "$1")
+  echo $(pactl list short sink-inputs | mawk -v check=${CLIENT_ID} '$3 == check {print $1}')
+}
+
+paClientOutputId() {
+  local CLIENT_ID=$(paClientId "$1")
+  echo $(pactl list short source-outputs | mawk -v check=${CLIENT_ID} '$3 == check {print $1}')
+}
+
+linkInputToSink() {
+  local INPUT=$(paInputSource "$1")
+  local OUTPUT="$2"
+  pactl load-module module-loopback source=${INPUT} sink=${OUTPUT}
+}
+
+moveClientSinks() {
+  local INPUT_ID=$(paClientInputId "$1")
+  local OUTPUT_ID=$(paSinkId "$2")
+  pactl move-sink-input ${INPUT_ID} ${OUTPUT_ID}
+  if [[ $? -gt 0 ]]; then
+    echo "Failed to connect $INPUT_ID to $OUTPUT_ID"
+  fi
+}
+
+checkClientExists() {
+	# TODO - write
+  local name="$1"
+  local id=$(paClientId "$name")
+  if [[ "$id" == "" ]]; then
+    echoerr "[ERROR] No client $name found."
+    exit 1
+  fi	
+}
+
+checkClientSinkExists() {
+  # TODO - write
+  local name="$1"
+  local id=$(paClientInputId "$name")
+  if [[ "$id" == "" ]]; then
+    echoerr "[ERROR] No client $name found."
+    exit 1
+  fi  
+}
+
+echoerr() {
+  echo 1>&2 "$@"  
+}
+
+# Sources, like microphone
+declare -a sources
+# Inputs which we do not want to hear on the speakers (like our own microphone)
+declare -a inputs
+# Inputs we want on the recording *AND* in our speakers
+declare -a monitored_inputs
+pa_setup=true
+
+
+usage() {
+  echo ""
+  echo "Usage:"
+  echo "$SCRIPT_NAME [-i input] [-mi monitored-input] [-s source] [-h]"
+  echo "    This script constructs the necessaary pulse-audio sinks"
+  echo "    for recording.  There are two sinks created: "
+  echo "     * recording_feed_out - for actually recording."
+  echo "     * comp_feed_out - for recording & playback through speakers."
+  echo ""
+  echo "    Inputs are passed via -i or -mi.  A monitored input is one "
+  echo "    you  wish to hear in the computer speakers."
+  echo "    Input arguments are passed via client name strings."
+}
+
+require_arg () {
+  local type="$1"
+  local opt="$2"
+  local arg="$3"
+  if [[ -z "$arg" ]] || [[ "${arg:0:1}" == "-" ]]; then
+    echo "$opt requires <$type> argument"
+    exit 1
+  fi
+}
+
+requireClientWithSink() {
+  local source="$1"
+  checkClientExists "$source"
+  checkClientSinkExists "$source"
+}
+
+requireSource() {
+  local source=$(paInputSource "$1")
+  if [[ "$source" == "" ]]; then
+    echoerr "Source $1 does not exist!"
+    exit 1
+  fi
+}
+
+addSource() {
+  requireSource "$1"
+  sources=( "${sources[@]}" "$1" )
+}
+
+addInput() {
+  requireClientWithSink "$1"
+  inputs=( "${inputs[@]}" "$1" )
+}
+
+addMonitoredInput() {
+  requireClientWithSink "$1"
+  monitored_inputs=( "${silent_inputs[@]}" "$1" )
+}
+
+process_args() {
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      -h|-help) usage; exit 1 ;;
+      -i|-input) require_arg "pulse audio client name" "$1" "$2" && addInput "$2" && shift 2 ;;
+      -mi|-monitored-input) require_arg "pulse audio client name" "$1" "$2" && addMonitoredInput "$2" && shift 2 ;;
+      -s|-source) require_arg "pulse audio source name" "$1" "$2" && addSource "$2" && shift 2 ;;
+      -rhythmbox) addMonitoredInput "rhythmbox" && shift ;;
+      -snowball) addSource "snowball" && shift ;;
+      -skip-setup) pa_setup=false && shift ;;
+      *) echoerr "Unknown argument: $1"; usage; exit 1 ;;
+    esac
+  done
+}
+
+
+# Basic setup
+
+#  silent_inputs---------------+--> recording_feed --> capture program   
+#                             /
+#  inputs ---->  comp_feed --+--> hardware_output
+
+basicSetup() {
+  echoerr "- Setting up recording + monitor feeds -"
+  # Create sinks
+  SINK_RECORDING_ID=$(pactl load-module module-null-sink sink_name="recording_feed_out" sink_properties=device.description="RecordingOUT")
+  SINK_COMP_ID=$(pactl load-module module-null-sink sink_name="comp_feed_out" sink_properties=device.description="CompFeedOUT")
+
+  echoerr "    * wiring monitor feed into local speakers "
+  # Hook the computer feed to the hardware speakers.
+  LINK_HW_TO_COMP=$(pactl load-module module-loopback source="comp_feed_out.monitor")
+
+  echoerr "    * hook monitor feed into recording feed"
+  # Hook the computer feed to the recording feed.
+  LINK_COMP_TO_RECORDING=$(pactl load-module module-loopback source="comp_feed_out.monitor" sink="recording_feed_out")
+}
+
+hookSourceInputImpl() {
+  echoerr "- Conecting input sources"
+  while [[ $# -gt 0 ]]; do 
+    echoerr "    * connecting $1..."
+    linkInputToSink "$1" "recording_feed_out"
+    shift
+  done
+}
+
+hookSourceInputs() {
+  hookSourceInputImpl "${sources[@]}"
+}
+
+hookMonitoredInputImpl() {
+  echoerr "- Conecting monitored inputs"
+  while [[ $# -gt 0 ]]; do 
+    echoerr "    * connecting $1..."
+    moveClientSinks "$1" "comp_feed_out"
+    shift
+  done
+}
+hookMonitoredInputs() {
+  hookMonitoredInputImpl "${monitored_inputs[@]}"
+}
+hookInputImpl() {
+  echoerr "- Conecting recorded inputs"
+  while [[ $# -gt 0 ]]; do 
+    echoerr "    + connecting $1..."
+    moveClientSinks "$1" "recording_feed_out"
+    shift
+  done
+}
+hookInputs() {
+  hookInputImpl "${inputs[@]}"
+}
+
+# TODO - Read inputs here and make sure we're recording something.
+
+process_args "$@"
+
+
+# Check sanity
+if [[ ${#sources[@]} -lt 1 ]] && [[ ${#inputs[@]} -lt 1 ]] && [[ ${#monitored_inputs[@]} -lt 1 ]]; then
+  echoerr "No inputs defined!"
+  usage
+  exit 1
+fi
+
+
+echo "Recording setup:  "
+echo "  recorded:  $sources $inputs $monitored_inputs"
+echo "  monitored: $monitored_inputs"
+
+if $pa_setup; then
+  basicSetup
+fi
+hookSourceInputs
+hookInputs
+hookMonitoredInputs
---
+++ twitch-stream.bash
@@ -0,0 +1,251 @@
+#!/bin/bash
+
+declare -r SCRIPT_NAME=${0##*/}
+
+# Requires
+# * avconv
+# * v4l
+# * v4l-utils
+
+# Static Configuration
+declare -r OUTPUT_RESOLUTION="1280x720" # Video Output Resolution
+declare -r FPS="24"                     # Frame per Seconds (Suggested 24, 25, 30 or 60)
+declare -r THREADS="4"                  # Change this if you have a good CPU (Suggested 4 threads, Max 6 threads)
+declare -r VIDEO_QUALITY="ultrafast"    # ultrafast, superfast, veryfast, faster, fast, medium, slow, slower, veryslow, placebo
+declare -r BITRATE="1000k"              # Constant bitrate (CBR) Increase this to get a better pixel quality (1000k - 3000k for twitch)
+declare -r AUDIO_RATE="44100"           # Twitch says it MUST have a 44k rate.
+
+
+# Discovered information from arguments
+declare input_resolution          # incoming resolution of the game
+declare twitch_server=live-jfk    # Twitch server to use (see http://bashtech.net/twitch/ingest.php)
+declare window_position           # Positioning of the window, auto-detected
+declare webcam_resolution=320:240 # incoming resolution of the webcam
+declare webcam_device             # device location for the webcam
+declare -a pulse_args             # Arguments for pulse audio script.
+declare overlay_image             # The overlay image to use for the stream.
+
+echoerr() {
+  echo 1>&2 "$@"  
+}
+
+# Finds one of the various ways you can configure your twitch key.
+findTwitchKey() {
+	LOCAL_KEY=$(cat .twitchkey)
+	HOME_DIR_KEY=$(cat ~/.twitchkey)
+	if [ "$STREAM_KEY" != "" ]; then
+		echo "$STREAM_KEY"
+	elif [ "$LOCAL_KEY" != "" ]; then
+		echo "$LOCAL_KEY"
+	elif [ "$HOME_DIR_KEY" != "" ]; then
+		echo "$HOME_DIR_KEY"
+	else
+		echoerr "No twitch key found! Please place one in either of the following: "
+		echoerr " * STREAM_KEY environment variable"
+		echoerr " * .twitchkey file in current directory"
+		echoerr " ~/.twitchkey file"
+		exit 1
+	fi
+}
+
+twitchStreamUrl() {
+	echo "rtmp://${twitch_server}.twitch.tv/app/$(findTwitchKey)"
+}
+
+setFullscreen() {
+	window_position="0,0"
+	input_resolution=$(xwininfo -root | awk '/geometry/ {print $2}'i | sed -e 's/\+[0-9]//g')
+}
+
+# TODO - This only limits the screen grab to coordinates, rather then the xid.
+selectWindow() {
+	echo "Click a window, dude"
+	xwininfo -stats > .record_window_info
+	window_position=$(cat .record_window_info | awk 'FNR == 8 {print $4}')","$(cat .record_window_info | awk 'FNR == 9 {print $4}')
+	input_resolution=$(cat .record_window_info | awk 'FNR == 12 {print $2}')"x"$(cat .record_window_info | awk 'FNR == 13 {print $2}')
+	rm -f .record_window_info 2> /dev/null
+	echo " "
+}
+
+selectWebcamError() {
+  echo "Please select on of the following webcams: "
+  echo "$(v4l2-ctl --list-device | grep /dev/)"
+}
+
+setWebcamResolution() {
+	# TODO - Validate input
+	webcam_resolution="$1"
+}
+
+createAvconvFilterString() {
+	if test -z "$overlay_image" ; then
+		# We do not have an overlay
+		if test -z "$webcam_device" ; then
+			echo "movie=$webcam_device:f=video4linux2 input_format=mjpeg, scale=$webcam_resolution , setpts=PTS-STARTPTS [WebCam];
+             [in] setpts=PTS-STARTPTS, [WebCam] overlay=main_w-overlay_w-10:10 [out]"
+		else
+			echo ""
+		fi
+	else
+		# We have an overlay		
+		if test -z "$webcam_device"; then
+			echo " movie=$overlay_image [OverlayPng];
+                   [in][OverlayPng] overlay=10:10 [out]"
+		else
+			echo " movie=$webcam_device:f=video4linux2, scale=$webcam_resolution , setpts=PTS-STARTPTS [WebCam]; 
+                   movie=$overlay_image [OverlayPng];
+                   [in][OverlayPng] overlay=10:10 [videoWithOverlay];
+                   [videoWithOverlay] setpts=PTS-STARTPTS, [WebCam] overlay=main_w-overlay_w-10:10 [out]"
+		fi
+	fi
+}
+
+streamTo(){
+	echo "Press Ctl-C to stop."
+	local filter_string=$(createAvconvFilterString)
+	if test -z "$filter_string"; then
+	  avconv \
+        -f x11grab \
+        -s $input_resolution \
+        -r "$FPS" \
+        -i :0.0+${window_position} \
+        -f alsa \
+        -i pulse \
+        -f flv -ac 2 -ar $AUDIO_RATE \
+        -vcodec libx264 \
+        -g $(($FPS*2)) \
+        -keyint_min $FPS \
+        -b $BITRATE \
+        -minrate $BITRATE \
+        -maxrate $BITRATE \
+        -pix_fmt yuv420p \
+        -s $OUTPUT_RESOLUTION \
+        -preset $VIDEO_QUALITY \
+        -tune film  \
+        -acodec libmp3lame \
+        -threads $THREADS \
+        -strict normal \
+        -bufsize $BITRATE \
+        "$1"
+	else
+      avconv \
+        -f x11grab \
+        -s $input_resolution \
+        -r "$FPS" \
+        -i :0.0+${window_position} \
+        -f alsa \
+        -i pulse \
+        -f flv -ac 2 -ar $AUDIO_RATE \
+        -vcodec libx264 \
+        -g $(($FPS*2)) \
+        -keyint_min $FPS \
+        -b $BITRATE \
+        -minrate $BITRATE \
+        -maxrate $BITRATE \
+        -pix_fmt yuv420p \
+        -s $OUTPUT_RESOLUTION \
+        -preset $VIDEO_QUALITY \
+        -tune film  \
+        -acodec libmp3lame \
+        -threads $THREADS \
+        -vf "$(createAvconvFilterString)" \
+        -strict normal \
+        -bufsize $BITRATE \
+        "$1"
+    fi
+}
+
+streamWithWebcamToTwitch() {
+	echo "Online! Check on http://twitch.tv/"
+    echo " "
+	streamWithWebcamTo "$(twitchStreamUrl)"
+}
+
+addPulseArg() {
+  pulse_args=( "${pulse_args[@]}" "$1" )
+}
+
+setTwitchServer() {
+	twitch_server="$1"
+}
+
+setWebcam() {
+	webcam_device="$1"
+}
+
+setOverlayImage() {
+	overlay_image="$1"
+}
+
+require_arg () {
+  local type="$1"
+  local opt="$2"
+  local arg="$3"
+  if [[ -z "$arg" ]] || [[ "${arg:0:1}" == "-" ]]; then
+    echo "$opt requires <$type> argument"
+    exit 1
+  fi
+}
+
+usage() {
+  echo ""
+  echo "Usage:"
+  echo "$SCRIPT_NAME [options]"
+  echo " This script records to twitch with the given inputs/outputs."
+  echo "   By default the entire screen is recorded and the default  "
+  echo "   pulse audio device is recorded."
+  echo
+  echo " Options:"
+  echo
+  echo "  -webcam <device>            Record the webcam."
+  echo "  -twitch-server <subdomain>  The twitch subdomain server"
+  echo "                              (default: live-jfk)"
+  echo "  -overlay <image>            An image to overlay on top of"
+  echo "                              the video, but under the webcam,"
+  echo "                              if the webcam is enabled."
+  echo "  -window                     On startup select a window to"
+  echo "                              set the xgrab coordinates."
+  echo "  -s <input>                  Record a given pulse audio source"
+  echo "                              on the audio stream."
+  echo "                              Example: 'snowball' would find any "
+  echo "                              snowball microphone connected."
+  echo "  -mi <pulse client>          Record any pulse-audio client"
+  echo "                              and also output to hardware speakers"
+  echo "                              Examples:  rhythmbox, wine"
+}
+
+processArgs() {
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      -h|-help) usage; exit 1 ;;
+      -window) selectWindow && shift ;;
+      -twitch-server) require_arg "twitch server" "$1" "$2" && setTwitchServer "$2" && shift 2 ;;
+	  -webcam) require_arg "webcamdevice" "$1" "$2" && setWebcam "$2" && shift 2 ;;
+      -overlay) require_arg "image file" "$1" "$2" && setOverlayImage "$2" && shift 2 ;;
+      *) addPulseArg "$1" && shift ;;
+    esac
+  done
+
+  # Default to full screen recording.
+  if [[ -z "$input_resolution" ]]; then 
+  	setFullscreen
+  fi
+}
+
+processArgs "$@"
+
+echo "Inputs "
+echo "========================================="
+echo "Webcam            :  $webcam_device"
+echo "Webcam Resolution :  $webcam_resolution"
+echo "Screen Start      :  $window_position"
+echo "Screen Resolution :  $input_resolution"
+echo "Twitch Server     :  $twitch_server"
+echo "Pulse Args        :  ${pulse_args[@]}"
+echo "Filter            :  $(createAvconvFilterString)"
+
+# We just make sure we can find it beofre setting up audio.
+ignore_me=$(findTwitchKey)
+
+# TODO - hook up audio correctly after setup
+streamTo "test.mp4"
